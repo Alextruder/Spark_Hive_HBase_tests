5db70c9d692febe1e8c74f76490b4cb46fd62e23
Alexandre Vignaud <44834499+Alextruder@users.noreply.github.com>
2021-10-13 15:49:53 +0200

    Update create_hivedb.scala

--- Test_Scripts/Spark/Basic_Tests/create_hivedb.scala
+++ Test_Scripts/Spark/Basic_Tests/create_hivedb.scala
@@ -3,5 +3,4 @@ sqlContext.sql("CREATE DATABASE IF NOT EXISTS company")
 sqlContext.sql("CREATE TABLE IF NOT EXISTS company.new_employee (id int, name string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LINES TERMINATED BY '\n'")
 sqlContext.sql("LOAD DATA LOCAL INPATH '/home/spark/Spark_Hive_HBase_tests/Test_Scripts/Spark/sample.txt' INTO TABLE company.new_employee")
 sqlContext.sql("SELECT * FROM company.new_employee WHERE id < 5").collect().foreach(println)
-
-:q
+System.exit(0)
