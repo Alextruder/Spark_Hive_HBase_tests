a051e48bf099230036806b56e612209729cff5dd
Alexandre Vignaud <44834499+Alextruder@users.noreply.github.com>
2021-10-13 19:50:06 +0200

    Update load_local.sh

--- Test_Scripts/Hive/Load_Local_Data/load_local.sh
+++ Test_Scripts/Hive/Load_Local_Data/load_local.sh
@@ -1,7 +1,7 @@
 echo 'Start Data injection'
 sleep 2s
 
-beeline jdbc:hive2:// <<EOF
+beeline -u jdbc:hive2:// <<EOF
 CREATE DATABASE company;
 CREATE TABLE company.employee (id int, name string, role string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LINES TERMINATED BY '\n' STORED AS textfile;
 LOAD DATA LOCAL INPATH '/home/hive/Spark_Hive_HBase_tests/Test_Scripts/Hive/sample.txt' INTO TABLE company.employee;
