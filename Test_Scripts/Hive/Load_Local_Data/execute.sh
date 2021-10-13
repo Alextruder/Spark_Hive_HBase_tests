925eb4e0d3192d3488cf9382bd6e0d5427b07207
Alexandre Vignaud <44834499+Alextruder@users.noreply.github.com>
2021-10-13 16:25:27 +0200

    Create execute.sh

---
+++ Test_Scripts/Hive/Load_Local_Data/execute.sh
@@ -0,0 +1,6 @@
+echo 'Start data injection in Hive'
+sleep 2s
+beeline -u jdbc:hive2:// -f /user/hive/Spark_Hive_HBase_tests/Test_Scripts/Hive/Load_Local_Data/load_local
+!quit
+sleep 2s
+echo 'End of data injection in Hive'
