4cf60731950d91de19784f65b9e4d6ef7a43056b
Alexandre Vignaud <44834499+Alextruder@users.noreply.github.com>
2021-11-03 08:49:29 +0100

    Update add_files.sh

--- Test_Scripts/Hadoop/add_files.sh
+++ Test_Scripts/Hadoop/add_files.sh
@@ -2,17 +2,17 @@ echo 'Start Test Hadoop'
 
 sleep 2s
 
-hdfs dfs -mkdir /user/hadoop/hadoop_logs
-hdfs dfs -put $HADOOP_HOME/logs/*.log /user/hadoop/hadoop_logs
+hdfs dfs -mkdir -p /user/data/hadoop/hadoop_logs
+hdfs dfs -put $HADOOP_HOME/logs/*.log  /user/data/hadoop/hadoop_logs
 echo ''
 echo 'Hadoop logs are:'
-hdfs dfs -ls /user/hadoop/hadoop_logs
+hdfs dfs -ls /user/data/hadoop/hadoop_logs
 
 echo ''
 echo ''
 
 echo 'Content of the first log'
-hdfs dfs -cat /user/hadoop/hadoop_logs/hadoop-root-namenode-*.log
+hdfs dfs -cat /user/data/hadoop/hadoop_logs/hadoop-root-namenode-*.log
 
 sleep 2s
 echo ''
