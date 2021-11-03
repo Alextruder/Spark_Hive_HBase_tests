5566f646f502a9a5cebf4f5235965075b51a3835
Alexandre Vignaud <44834499+Alextruder@users.noreply.github.com>
2021-11-03 08:50:48 +0100

    Update load_local.sh

--- Test_Scripts/Hive/Load_Local_Data/load_local.sh
+++ Test_Scripts/Hive/Load_Local_Data/load_local.sh
@@ -4,7 +4,7 @@ sleep 2s
 beeline -u jdbc:hive2:// <<EOF
 CREATE DATABASE company;
 CREATE TABLE company.employee (id int, name string, role string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ' ' LINES TERMINATED BY '\n' STORED AS textfile;
-LOAD DATA LOCAL INPATH 'Movies-Analytics/Test_Scripts/Hive/sample.txt' INTO TABLE company.employee;
+LOAD DATA LOCAL INPATH '/home/data/Movies-Analytics/Test_Scripts/Hive/sample.txt' INTO TABLE company.employee;
 SELECT * FROM company.employee;
 EOF
 
